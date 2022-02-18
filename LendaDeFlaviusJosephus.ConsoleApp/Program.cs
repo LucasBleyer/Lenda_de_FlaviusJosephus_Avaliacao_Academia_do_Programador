4ea0441e16fe4bea9a98d881cca68b82b2c763f2
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-18 16:08:35 -0300

    add LendaDeFlaviusJosephus

--- LendaDeFlaviusJosephus.ConsoleApp/Program.cs
+++ LendaDeFlaviusJosephus.ConsoleApp/Program.cs
@@ -6,7 +6,12 @@ namespace LendaDeFlaviusJosephus.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe o número de pessoas presentes no círculo: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o tamanho do salto de um homem até o próximo homem que será morto: ");
+            int k = Convert.ToInt32(Console.ReadLine());    
+
         }
     }
 }
